5a4b7c038ff2676b5e5a7e2fb217b8df0b1b6821
itsmaheshkariya <itsmaheshkariya@gmail.com>
2020-12-21 05:52:54 -0800

    basics of js

---
+++ node/middleware.js
@@ -0,0 +1,90 @@
+// let a = ['mun','tun','sun','kaju','ramu'];
+// console.log(a.join(" "))
+
+// some,every,find
+
+
+// let l1 = [1,2,3]
+// let a = l1.some((item) => {
+//     return item < 5;
+// });
+
+
+// let l1 = [1,2,3]
+// let a = l1.every((item) => {
+//     return item < 5;
+// });
+
+let l1 = [1,2,3]
+// let a = l1.find((item) => {
+//     return item < 5;
+// });
+
+
+//console.log(l1.reduceRight());
+
+
+// cloud.google.com
+// https://download.visualstudio.microsoft.com/download/pr/acff3e6a-d8d6-4c2a-a0cb-1853b58055cc/7910b2a414caa17d30b0cb82583cb542/dotnet-sdk-5.0.101-win-x64.exe
+
+// function print(x){
+//     return console.log(x)
+// }
+// let str =  "mahesh dipak kariya"
+// console.log(str.split(" "))
+
+// let b = a.toString().replace(/,/g, ' ').split();
+// console.log(a);
+// cosnole.log(a.toString())
+
+//let b = a.concat('suraj');                      // we can add items using concat also
+// let b = a.slice(1);                               // slice() creates new array from selected item
+// a.splice(0,2,'kanchana','maithali');
+// console.log(a.reverse());
+// console.log(a.sort());
+// console.log(a.indexOf('mun'));
+
+
+
+// console.log(a.copyWithin(1,2));
+// console.log(b);
+// a[10] = 'tomey';
+// // let b = ['1','2','3','4','5'];
+// // let c = a.concat(b);
+// print(a);
+// // a.push('run');
+// // a.unshift('gun');
+// // a.pop();
+// // a.shift();
+// // a.splice(1,1);
+// // console.log(a);
+
+// // a.forEach((item, index) =>{
+// //     console.log(item,index);
+// // });
+
+// // a.map((item)=>{
+// //     console.log(item);
+// // })
+
+// // let b = a.reduce((total,item)=>{
+// //     return total * item;
+// // })
+
+
+// // let a = ['1','20','50','20'];
+// // let c = a.filter((item)=> {
+// //     return item>10;
+// // });
+// // console.log(c);
+
+
+// // console.log(b);
+
+// // console.log(a);
+// // let arr = [ "mahesh ", "kariya" ]
+// // console.log(arr.join());
+
+
+
+
