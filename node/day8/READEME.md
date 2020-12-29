afb689269f0b5bead9068f893f36704c1952f44c
itsmaheshkariya <itsmaheshkariya@gmail.com>
2020-12-29 04:02:39 -0800

    README.md

---
+++ node/day8/READEME.md
@@ -0,0 +1,74 @@
+## 
+`Request Object and its methods`
+```js
+req.body
+req.params
+req.url
+req.headers
+req.query
+```
+
+`Response Object and its methods`
+```js
+res.render
+res.send
+res.redirect
+res.json
+res.status
+res.end
+```
+
+
+`Application Object and its methods`
+```js
+app.get
+res.post
+res.put
+res.delete
+res.listen
+res.engine
+```
+
+
+`Router Object and its methods`
+```js
+router.get
+router.post
+router.put
+router.delete
+router.all
+router.use
+router.param
+```
+
+
+`Controllers directory and your first controller file`
+```js
+
+```
+
+
+`Write business logic in your controller file`
+```js
+
+```
+
+`Routes directory and your first router file`
+```js
+
+```
+
+`Write your routes inside this file`
+```js
+
+```
+
+`Use routes from app.js file`
+```js
+
+```
+
+`Config environments`
+```js
+
+```
--- node/day8/new.js
+++ node/day8/new.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+// https://github.com/itsmaheshkariya/dscode/tree/master/node/day8
+
+
+const bodyParser = require("body-parser")
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/",require("./users/user1"))
+
+
+
+
+app.listen(8080);
+
+
+// https://www.youtube.com/channel/UCDR79YG43mX3I8_75kyd3Kw
+
+// localhot:8080
+
 // // localhost:8080/123/mahesh
 // app.get("/:id/:name/",(req,res)=>{
 //     res.send(req.params.id)
@@ -13,21 +34,3 @@ const app = express();
 
 
 
-
-app.listen(process.env.PORT);
-
-
-
-// localhot:8080
-
-
-
-
-// const bodyParser = require("body-parser")
-
-// // https://www.youtube.com/channel/UCDR79YG43mX3I8_75kyd3Kw
-
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({extended: true}));
-
-// // app.use("/",require("./users/user1"))
